89181d1d2e089419caa99fe791d32eefb30dae50
Xiaoyang Kao <kaoxiaoyang@gmail.com>
2013-09-16 01:57:32 +0800

    First commit

---
+++ about.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>TASMUN - Model United Nations</title>
+		
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+		<link rel="stylesheet" href="./static/bootstrap.min.css">			
+		<link rel="stylesheet/less" type="text/css" href="style.less"/>
+
+		<script src="//cdnjs.cloudflare.com/ajax/libs/less.js/1.4.1/less.min.js"></script>
+		<script src="./bootstrap3/js/transition.js"></script>
+		<script src="./bootstrap3/js/collapse.js"></script>
+		<script src="./bootstrap3/js/dropdown.js"></script>
+		<script src="./bootstrap3/js/carousel.js"></script>
+		<link id="favicon" rel="shortcut icon" type="image/x-icon" href="./static/favicon.png" />
+
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Fjalla+One' rel='stylesheet' type='text/css'>
+ 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+		<!--[if lt IE 9]><script src="http://xy-kao.com/theme/new/html5shiv.js"></script><![endif]-->
+	</head>
+
+	<body>
+		<?php include('./header.php'); ?>
+
+		<div class="container">
+		   <div class="row" style='padding-top:15px;'>
+		      <div id="carousel" class="carousel slide">
+		        <div class="carousel-inner">
+		            <div class="item active">
+		               <img src="http://placehold.it/970x400">
+		            </div>
+		            <div class="item">
+		               <img src="http://placehold.it/970x400">
+		            </div>
+		            <div class="item">
+		               <img src="http://placehold.it/970x400">
+		            </div>
+		         </div>
+		      </div>
+		   </div>
+		</div>
+
+		<script>
+			$('.carousel').carousel({
+			  interval: 4000
+			});
+		</script>
+
+	</body>
+</html>
---
+++ header.php
@@ -0,0 +1,56 @@
+		<header>
+			<div class="container">
+				<div class="row" style=''>
+					<div class="col-md-4">
+						<img id='logo' src='logo4.png'>
+					</div>
+					<div class="col-md-8" style="text-align: right;">
+						<h1 style="color: #D1E2F0;margin: 0;margin-top: 46px;font-family: Fjalla One;font-size: 21pt;">April 27-28, 2014</h1>
+					</div>
+				</div>
+			</div>
+		</header>
+
+		<nav class="navbar navbar-default" role="navigation" style='font-family: Arial;font-size: 13pt;margin-bottom:0;'>
+			<div class='container'>
+				<ul class="nav navbar-nav">
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle white">About TASMUN<b class="caret"></b>
+						</a>
+						<ul class="dropdown-menu">
+							<li><a href="#">Schedule</a></li>
+							<li><a href="#">FAQ</a></li>
+						</ul>
+					</li>
+
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle white">Registration<b class="caret"></b>
+						</a>
+						<ul class="dropdown-menu">
+							<li><a href="#">Process</a></li>
+							<li><a href="#">Conference Fee</a></li>
+							<li><a href="#">Forms</a></li>
+						</ul>
+					</li>
+
+					<li>
+						<a href="#" class='white'>Lodging</a>
+					</li>
+
+					<li>
+						<a href="#" class='white'>Transportation</a>
+					</li>
+
+					<li>
+						<a href="#" class='white'>Country Matrix</a>
+					</li>
+
+					<li>
+						<a href="#" class='white'>Press Blog</a>
+					</li>
+
+				</ul>
+
+
+			</div>
+		</nav>
